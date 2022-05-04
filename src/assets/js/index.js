3b529020227fdb0a8fcbd5f0c059f1367b9e4bbd
JohnCMello <johncharlesmello@gmail.com>
2022-05-04 10:56:32 -0300

    Refactor:
    Create function createTodoInfo
    
    Create function createTodoElementsFromLocalStorage
    
    Create function createTodoElements

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -8,61 +8,75 @@ const $toDoItems = document.querySelectorAll(`[data-js=todo]`) ;
 
 // Functions ==================================================================
 
-const getTodosFromLocalStorage = () => JSON.parse(localStorage.getItem("todo-list")) || [];
-
-const createTodoElements = () => {
-    const todos = getTodosFromLocalStorage() ;
+const createTodoInfo = () => {
+    const textValue = $taskText.value.trim();
     
-    const todo_el = document.createElement(`li`);
-        
-    todos.forEach(todo =>{
-
-        todo_el.dataset.js = `todo`;
-        todo_el.innerText = todo.name;
-        todo_el.addEventListener(`click`, toggleToDos)
-        todo_el.classList.add(`todo`, todo.status)
-        todo_el.setAttribute(`id`, todo.id)
-
-        $toBeDoneList.appendChild(todo_el);
-        
-    })
-    
-}
-
-const setTodosToLocalStorage = () =>{
-    let textValue = $taskText.value;
-
-    const todos = getTodosFromLocalStorage();
-
-    let todoInfo = {
+    const todoInfo = {
         _id: Math.floor(Math.random()*1000), 
         name: textValue, 
         status:`pending`
     };
+    return todoInfo
+}
+
+const setTodosToLocalStorage = (todoInfo) =>{
+    const todos = getTodosFromLocalStorage();
 
     todos.push(todoInfo)
 
     localStorage.setItem("todo-list", JSON.stringify(todos))
 };
 
+const getTodosFromLocalStorage = () => JSON.parse(localStorage.getItem("todo-list")) || [];
+
+const createTodoElementsFromLocalStorage = () => {
+    
+    const todos = getTodosFromLocalStorage();
+    
+    todos.forEach( (todo) => {
+        const todo_el = document.createElement(`li`);
+
+        todo_el.dataset.js = `todo`;
+        todo_el.innerText = todo.name;
+        todo_el.addEventListener(`click`, toggleToDos)
+        todo_el.classList.add(`todo`, todo.status)
+        todo_el.setAttribute(`id`, todo._id)
+
+        $toBeDoneList.append(todo_el);
+    })
+};
+
+const createTodoElements = (todoInfo) =>{
+    const todoData = todoInfo
+
+    const todo_el = document.createElement(`li`);
+
+    todo_el.dataset.js = `todo`;
+    todo_el.innerText = todoData.name;
+    todo_el.addEventListener(`click`, toggleToDos)
+    todo_el.classList.add(`todo`, todoData.status)
+    todo_el.setAttribute(`id`, todoData._id)
+
+    $toBeDoneList.append(todo_el);
+    
+    $taskText.value = ``;
+};
+
 const addTodos = (e)=>{
     e.preventDefault();
-    setTodosToLocalStorage()
-    createTodoElements()
-
-    $taskText.value = ``;
+    setTodosToLocalStorage(createTodoInfo())
+    createTodoElements(createTodoInfo())
 };
 
 const toggleToDos = (e) =>{
     const todo = e.target;
     todo.classList.toggle(`completed`)
-
 };
 
-createTodoElements()
+createTodoElementsFromLocalStorage()
+
 // Listeners ==================================================================
 
-// Add to-dos ###
+// Add to-dos
 $toDoForm.addEventListener(`submit`, addTodos)
-
 
